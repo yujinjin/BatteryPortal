940957d483a8995216cf2b470f7d67d120e062c2
jackyu <jackyu@DESKTOP-KOT7AGF>
2018-09-12 14:59:17 +0800

    项目重新整理

--- src/assets/js/controller/product-details.js
+++ src/assets/js/controller/product-details.js
@@ -4,12 +4,51 @@
  * 描述：商品详情
  */
 import "../site";
+import "../lib/jqzoom/jquery.jqzoom";
 import "../../style/product-details.less";
+import "../lib/jqzoom/jquery.jqzoom.css";
 
 $(function(){
 	let productDetails = {
 		init(){
-			
+			$(".sku-wrap a").on("click", function(e){
+				if(!$(this).hasClass("active")) {
+					$(".sku-wrap a.active").removeClass("active");
+					$(this).addClass("active");
+					$(".jqzoom > img").attr("src", $(this).find("img").attr("src"));
+					$(".jqzoom > img").attr("jqimg", $(this).find("img").attr("jqimg"));
+				}
+			});
+			$(".spec-scroll > .control").on("click", function(e){
+				if($(".sku-wrap li").length === 1) return;
+				if($(this).hasClass(".prev")) {
+					// 上一个
+					if($(".sku-wrap a.active").parent("li").prev("li").length == 1){
+						$(".sku-wrap a.active").parent("li").prev("li").find("a").trigger("click");
+					} else {
+						$(".sku-wrap li:last").find("a").trigger("click");
+					}
+				} else {
+					// 下一个
+					if($(".sku-wrap a.active").parent("li").next("li").length == 1){
+						$(".sku-wrap a.active").parent("li").next("li").find("a").trigger("click");
+					} else {
+						$(".sku-wrap li:first").find("a").trigger("click");
+					}
+				}
+			});
+			$(".jqzoom").jqueryzoom({xzoom:250,yzoom:250});
+//			$('.jqzoom').jqzoom({
+//              zoomType: 'standard',
+//				lens: true,
+//				preloadImages: false,
+//				alwaysOn: false,
+//				zoomWidth: 250,
+//				zoomHeight: 250,
+//				xOffset: 120,
+//				yOffset: 30,
+//		        title: false
+//          });
 		}
 	}
 	productDetails.init();
